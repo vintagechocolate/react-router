17afd353bd4859f72d048ca576d52f204818005c
vintagechocolate <vintagechocolate@gmx.us>
2024-05-13 16:30:55 -0400

    0.0.1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './components/Home';
@@ -11,11 +11,11 @@ function App() {
     <Router>
       <div>
         <Header />
-        <Switch>
+        <Routes>
           <Route exact path="/" component={Home} />
           <Route path="/about" component={About} />
           <Route path="/contact" component={Contact} />
-        </Switch>
+        </Routes>
         <Footer />
       </div>
     </Router>
