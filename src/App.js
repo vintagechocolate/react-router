fc38e447a0192e55582ff092bebafb3a849a404f
vintagechocolate <vintagechocolate@gmx.us>
2024-05-14 10:49:09 -0400

    last update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Header from './components/Header';
+import Header from  './components/Header';
 import Footer from './components/Footer';
 import Home from './components/Home';
 import About from './components/About';
@@ -12,9 +12,9 @@ function App() {
       <div>
         <Header />
         <Routes>
-          <Route exact path="/" component={Home} />
-          <Route path="/about" component={About} />
-          <Route path="/contact" component={Contact} />
+          <Route path="/" element={<Home/>} />
+          <Route path="/about" element={<About/>} />
+          <Route path="/contact" element={<Contact/>} />
         </Routes>
         <Footer />
       </div>
